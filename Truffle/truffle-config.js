cf58714ccdea42450103b44633da12bbf8e894bb
Julie Jarzemsky <jjarzem@gmail.com>
2022-03-21 10:05:33 -0600

    Add Infura to Truffle, fix dotenv issue, add in contracts.json (goes with previous commit updating contract)

--- Truffle/truffle-config.js
+++ Truffle/truffle-config.js
@@ -1,43 +1,30 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const MNEMONIC = process.env.MNEMONIC;
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
   networks: {
     develop: {
       host: "127.0.0.1",
       port: 7545,
       chainId: 1337,
-      network_id: "*",
-      deploymentPollingInterval: 10,
+      network_id: "1337",
+      deploymentPollingInterval: 10
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`),
+      network_id: 4,
+      gas: 8500000,
+      gasPrice: 1000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    }
   },
   compilers: {
     solc: {
       version: "^0.8.0"
     }
-  }
-  //
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: true,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "sqlite",
-  //     settings: {
-  //       directory: ".db",
-  //     },
-  //   },
-  // },
+  },
 };
